e9ab327796d469ea8cc147a18b132d495206b065
mshan10 <mshan@nd.edu>
2019-03-31 12:01:10 -0400

    fixed main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -29,9 +29,21 @@ export class MainComponent implements OnInit {
     this.month = false;
     this.cal1 = true;
     this.date = false;
+    this.cal2 = false;
+  }
+
+  onMonth() {
+    this.month = true;
+    this.week = false;
+    this.cal1 = false;
+    this.cal2 = false;
+    this.date = true;
   }
   onCheck() {
     this.cal2 = true;
     this.cal1 = false;
+    this.month = false;
+    this.date = false;
+    this.week = true;
   }
 }
