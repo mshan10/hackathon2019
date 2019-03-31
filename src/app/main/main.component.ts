3f63031aba63e23988b36d46647cf0a3776da996
mshan10 <mshan@nd.edu>
2019-03-31 00:09:02 -0400

    added main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,14 +8,36 @@ import { Router } from '@angular/router';
 })
 export class MainComponent implements OnInit {
   mcwell = false;
+  // panda = true;
+  blank = true;
+  date = false;
+  cal1 = false;
+  cal2 = false;
+
+  month = true;
+  week = false;
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  onMcwell(event) {
-    console.log('mcwell clicked')
-    this.mcwell = true;
-  }
+  onAnnounce() {
 
+  }
+  
+  onMcwell() {
+    this.date = true;
+    this.blank = false;
+  }
+ 
+  onWeek() {
+    this.week = true;
+    this.month = false;
+    this.cal1 = true;
+    this.date = false;
+  }
+  onCheck() {
+    this.cal2 = true;
+    this.cal1 = false;
+  }
 }
